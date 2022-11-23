8125bb51101be4141c37b8baa5362d3c5add45e7
Marc <marcalsinac@gmail.com>
2022-11-23 12:49:45 +0100

    Fix cambiat la manera de com es calcula la gr, tenint en compte totes les particules amb totes les particules

--- analysis_m.F90
+++ analysis_m.F90
@@ -2,7 +2,7 @@ module analysis_m
     use, intrinsic :: iso_fortran_env, only: dp => real64, i64 => int64
     implicit none
 
-    public :: g_r
+    public :: g_r, calc_msd
 
 contains
 
@@ -17,21 +17,24 @@ contains
         real(kind=dp), dimension(:,:)                :: pos
         real(kind=dp), intent(in)                    :: dens, max_dist
         ! Internal variables
-        integer(kind=i64)                            :: n_bins, i_ax, index_mat
+        integer(kind=i64)                            :: n_bins, i_ax, index_mat, j_ax, n_p
         real(kind=dp)                                :: dr, dist
         real(kind=dp), parameter                     :: PI = acos(-1.0_dp)
 
         n_bins = size(gr_mat, dim=2, kind=i64)
+        n_p = size(pos, dim=1, kind=i64)
         dr = max_dist / real(n_bins, kind=dp)
 
-        gr_mat(1,:) = [(real(i_ax, dp)*dr, i_ax=0, n_bins-1)]
+        gr_mat(1,:) = [(real(i_ax, dp)*dr, i_ax=1, n_bins)]
         gr_mat(2,:) = 0.0_dp
 
         ! Calculem n(r)
-        do i_ax = 2, size(pos, dim=1)
-            dist = norm2(pos(1,:) - pos(i_ax,:))
-            index_mat = ceiling(dist/dr)
-            gr_mat(2, index_mat) = gr_mat(2, index_mat) + 1.0_dp
+        do j_ax = 1, n_p
+            do i_ax = 2, n_p
+                dist = norm2(pos(j_ax,:) - pos(i_ax,:))
+                index_mat = ceiling(dist/dr)
+                gr_mat(2, index_mat) = gr_mat(2, index_mat) + 1.0_dp
+            end do
         end do
 
         ! Calculem g(r)
@@ -42,6 +45,30 @@ contains
         gr_mat(2,:) = gr_mat(2,:) * (1.0_dp / (dens*4.0_dp*PI))
 
     end subroutine g_r
+
+    subroutine calc_msd(msd_vec, pos)
+        ! Necessito guardar les posicions per a cada particula per cada frame
+        implicit none
+        ! In/Out variables
+        real(kind=dp), dimension(:), intent(out)    :: msd_vec
+        real(kind=dp), dimension(:,:,:), intent(in) :: pos
+        ! Internal variables
+        integer(kind=i64)                           :: num_frames, i_frame, j_part, n_p
+        real(kind=dp)                               :: dd
+
+        num_frames = size(msd_vec, dim=1)
+        n_p = size(pos, dim=2)
+        msd_vec = 0.0_dp
+
+        do i_frame = 2, num_frames
+            do j_part = 1, n_p
+                dd = norm2(pos(i_frame, j_part, :) - pos(1, j_part, :)) ** 2
+                msd_vec(i_frame) = msd_vec(i_frame) + dd
+            end do
+            msd_vec(i_frame) = msd_vec(i_frame) / real(n_p, kind=dp)
+        end do
+
+    end subroutine calc_msd
 
 end module analysis_m
 
